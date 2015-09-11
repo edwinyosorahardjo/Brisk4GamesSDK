0f16f67683d5ae30443442737a70d9f957c38c4f
Andy Cross <andy@elastacloud.com>
2015-09-11 11:09:10 +0100

    changes for docuentation

--- Brisk4GameSDK/Asset.cs
+++ Brisk4GameSDK/Asset.cs
@@ -17,26 +17,32 @@ namespace Brisk4GameSDK
         /// Gets the Filename
         /// </summary>
         /// <see cref="System.String"/>
+        /// <returns>System.String</returns>
         public String Filename { get; }
         /// <summary>
         /// Gets a TenantName
         /// </summary>
+        /// <returns>System.String</returns>
         public String TenantName { get; }
         /// <summary>
         /// The name of the Game
         /// </summary>
+        /// <returns>System.String</returns>
         public String GameName { get; }
         /// <summary>
         /// The UserName associated with this Asset
         /// </summary>
+        /// <returns>System.String</returns>
         public String UserName { get; }
         /// <summary>
         /// An endpoint location on the CDN for this asset
         /// </summary>
+        /// <returns>System.String</returns>
         public String CdnEndpoint { get; }
         /// <summary>
         /// The Root Container
         /// </summary>
+        /// <returns>System.String</returns>
         public String RootContainer { get;  }
     }
 }
--- Brisk4GameSDK/FriendLink.cs
+++ Brisk4GameSDK/FriendLink.cs
@@ -12,6 +12,10 @@ namespace Brisk4GameSDK
     /// </summary>
     public class FriendLink
     {
+        /// <summary>
+        /// The Id of the Player
+        /// </summary>
+        /// <returns>System.String</returns>
         public String PlayerId { get; set; }
         public String FriendId { get; set; }
         public String GameId { get; set; }
